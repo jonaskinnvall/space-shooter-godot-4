78ee9c74abf6da53fad5ce19701d3a746706897f
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-24 23:52:19 +0100

    feat: add sound effects to effects and laser

--- effect_creator.gd
+++ effect_creator.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 @export var EffectScene: PackedScene
+var height = ProjectSettings.get_setting('display/window/size/viewport_height')
+var width = ProjectSettings.get_setting('display/window/size/viewport_width')
 
 func _exit_tree():
 	if not EffectScene is PackedScene: return
+	if global_position.x < 0 or global_position.x > width: return
+	if global_position.y < 0 or global_position.y > height: return
 	
 	var world = get_tree().current_scene
 	var effect = EffectScene.instantiate()
