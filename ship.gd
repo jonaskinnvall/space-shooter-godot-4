50d99f05df443a582dea426b7092fae062df4255
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-28 23:15:18 +0100

    feat: change to game over scene when ship crashes
    - Ship emits signal that world listens to, awaits a 1 sec timer and then
      changes scene to game over

--- ship.gd
+++ ship.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var speed = 100
 const LASER_SCENE = preload("res://laser.tscn")
 
+signal destroyed
+
 func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed*delta
@@ -18,3 +20,4 @@ func _process(delta):
 func _on_area_entered(area):
 	area.queue_free()
 	queue_free()
+	destroyed.emit()
--- world.gd
+++ world.gd
@@ -8,3 +8,11 @@ var score = 0:
 	set(value):
 		score = value
 		score_label.text = "Score: " + str(score)
+		
+
+
+
+func _on_ship_destroyed():
+	await get_tree().create_timer(1.0).timeout
+	
+	get_tree().change_scene_to_file("res://game_over_screen.tscn")
