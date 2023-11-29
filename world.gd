a987cf64ae590b623b1880944956a442f0c7933c
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-29 07:55:03 +0100

    fix: save new highscore on ship crash if higher than previous

--- world.gd
+++ world.gd
@@ -15,4 +15,10 @@ var score = 0:
 func _on_ship_destroyed():
 	await get_tree().create_timer(1.0).timeout
 	
+	var highscore = SaveLoad.load_score()
+	if highscore != null:
+		if score > highscore:
+			SaveLoad.save_score(score)
+	else: SaveLoad.save_score(score)
+	
 	get_tree().change_scene_to_file("res://game_over_screen.tscn")
