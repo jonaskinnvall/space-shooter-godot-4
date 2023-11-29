d60ee214de0814222bff933f37a9e482175e612e
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-29 07:56:02 +0100

    fix: update game_over_screen to display highscore

--- game_over_screen.gd
+++ game_over_screen.gd
@@ -1,5 +1,14 @@
 extends ColorRect
 
+@onready var highscore_label = $HighscoreLabel
+
+
+func _ready():
+	var highscore = SaveLoad.load_score()
+	if highscore == null: return 
+	
+	highscore_label.text = "Highscore: " + str(highscore)
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
