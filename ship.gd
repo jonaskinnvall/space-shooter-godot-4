cb182002a2b9c444b23a94861962c6732a20fb99
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-11-29 08:07:25 +0100

    fix: prevent player from moving outside of view

--- ship.gd
+++ ship.gd
@@ -1,15 +1,17 @@
 extends Area2D
 
-@export var speed = 100
-const LASER_SCENE = preload("res://laser.tscn")
-
 signal destroyed
+const LASER_SCENE = preload("res://laser.tscn")
+@export var speed = 100
+var height = ProjectSettings.get_setting('display/window/size/viewport_height')
 
 func _process(delta):
 	if Input.is_action_pressed("ui_down"):
-		position.y += speed*delta
+		if position.y < height - 8:
+			position.y += speed * delta
 	if Input.is_action_pressed("ui_up"):
-		position.y -= speed*delta
+		if position.y > 0 + 8:
+			position.y -= speed * delta
 	if Input.is_action_just_pressed("ui_accept"):
 		var laser = LASER_SCENE.instantiate()
 		var world = get_tree().current_scene
